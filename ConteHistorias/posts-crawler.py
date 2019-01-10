fc3cb0106109d752753ee6ba4e87d2b8003f2889
Dyego Alekssander Maas <dyego.maas@hbsis.com.br>
2019-01-10 13:27:05 -0200

    Implementing pagination.

--- ConteHistorias/posts-crawler.py
+++ ConteHistorias/posts-crawler.py
@@ -5,4 +5,13 @@ class BlogSpider(scrapy.Spider):
     start_urls = ['https://contehistorias.com']
 
     def parse(self, response):
-        categories = response.css('aside.widget_categories ul > li > a::attr(href)').extract()
+        post_urls = response.css('article > header > h2 > a::attr(href)').extract()
+        for post_url in post_urls:
+            yield scrapy.Request(url = post_url, callback = parse_post)            
+        
+        next_url = response.css('nav.posts-navigation > div.nav-links > div.nav-previous > a::attr(href)').extract_first()
+        if next_url:
+            yield scrapy.Request(url = next_url, callback = self.parse)
+
+    def parse_post(self, response):
+        yield 'x' # TODO parse post :)
