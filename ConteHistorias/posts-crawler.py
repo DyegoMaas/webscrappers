da0217361ef08926c6cd0b97b672ad5fd3e66ffb
Dyego Alekssander Maas <dyego.maas@gmail.com>
2019-01-10 21:46:27 -0200

    Crawling more information on the blog's posts.

--- ConteHistorias/posts-crawler.py
+++ ConteHistorias/posts-crawler.py
@@ -1,4 +1,5 @@
 import scrapy
+import datetime
 
 class BlogSpider(scrapy.Spider):
     name = 'ConteHistoriasSpider'
@@ -7,11 +8,30 @@ class BlogSpider(scrapy.Spider):
     def parse(self, response):
         post_urls = response.css('article > header > h2 > a::attr(href)').extract()
         for post_url in post_urls:
-            yield scrapy.Request(url = post_url, callback = parse_post)            
+            yield scrapy.Request(url = post_url, callback = self.parse_post)            
         
         next_url = response.css('nav.posts-navigation > div.nav-links > div.nav-previous > a::attr(href)').extract_first()
         if next_url:
             yield scrapy.Request(url = next_url, callback = self.parse)
 
     def parse_post(self, response):
-        yield 'x' # TODO parse post :)
+        post_title = response.css('h2.entry-title::text').extract_first()
+        post_title_parts = post_title.split('|') # pode não haver | no começo
+        post_prefix = post_title_parts[0].strip() if len(post_title_parts) > 1 else ''
+        post_name = post_title_parts[1].strip() if len(post_title_parts) > 1 else post_title_parts[0].strip()
+
+        url_parts = response.url.split('/')
+        publish_year = int(url_parts[3])
+        publish_month =int(url_parts[4])
+        publish_day = int(url_parts[5])
+
+        yield {
+            'title': {
+                'original_value': post_title,
+                'prefix': post_prefix,
+                'name': post_name
+            },
+            'minutes_to_read': int(response.css('span.wtr-time-number::text').extract_first()),
+            'author': response.css('span.author > a::text').extract_first(),
+            'publish_date': datetime.date(publish_year, publish_month, publish_day)
+        }
